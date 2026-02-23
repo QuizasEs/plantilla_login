bb1942ec30a91bfd8b4443b24e2ffcf1975969b6
QuizasEs <132032588+QuizasEs@users.noreply.github.com>
2026-02-22 20:58:05 -0400

    error de login solucionado y plantilla adjuntada

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,60 +1,31 @@
 <?php
-if ($peticionAjax) {
-    require_once "../models/loginModel.php";
-} else {
-    require_once "./models/loginModel.php";
-}
+require_once "./models/loginModel.php";
 
 class loginController extends loginModel
 {
     /* controlador para iniciar sesion */
     public function iniciar_sesion_controller()
     {
+        session_start(['name' => 'SMP']);
+        
         $usuario = mainModel::limpiar_cadena($_POST['Usuario_log']);
         $contraseña = mainModel::limpiar_cadena($_POST['Password_log']);
 
         /* == comprobar que los campos no se encuentren vacios == */
         if ($usuario == "" || $contraseña == "") {
-            echo '
-                    <script>
-                        Swal.fire({
-                            title: "Ocurrio un error inesperado",
-                            text: "No se han llenado todos los campos obligatorios!",
-                            icon: "error",
-                            confirmButtonText: "Aceptar"
-                        });
-                    </script>
-                ';
-            exit();
+            $_SESSION['error_login'] = 'No se han llenado todos los campos obligatorios!';
+            return 'error';
         }
 
         /* =verificar la integridad de los datos== */
-        if (mainModel::verificar_datos("^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ_]{3,100}", $usuario)) {
-            echo '
-                    <script>
-                        Swal.fire({
-                            title: "Ocurrio un error inesperado",
-                            text: "El NOMBRE DE USUARIO no coincide con el formato solicitado!",
-                            icon: "error",
-                            confirmButtonText: "Aceptar"
-                        });
-                    </script>
-                ';
-            exit();
+        /*if (mainModel::verificar_datos("^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ_]{3,100}", $usuario)) {
+            $_SESSION['error_login'] = 'El NOMBRE DE USUARIO no coincide con el formato solicitado!';
+            return 'error';
         };
         if (mainModel::verificar_datos("[A-Za-zÁÉÍÓÚáéíóúÑñ0-9@$!%*?&._#]{3,100}", $contraseña)) {
-            echo '
-                    <script>
-                        Swal.fire({
-                            title: "Ocurrio un error inesperado",
-                            text: "La CONTRASEÑA no coincide con el formato solicitado!",
-                            icon: "error",
-                            confirmButtonText: "Aceptar"
-                        });
-                    </script>
-                ';
-            exit();
-        };
+            $_SESSION['error_login'] = 'La CONTRASEÑA no coincide con el formato solicitado!';
+            return 'error';
+        };*/
         $contraseña = mainModel::encryption($contraseña);
 
         $datos_login = [
@@ -67,26 +38,15 @@ class loginController extends loginModel
             /* =============iniciamos las variables de session=============== */
             $row = $datos_cuenta->fetch();
 
-            session_start(['name' => 'SMP']);
-
             $_SESSION['id_smp'] = $row['us_id'];
             $_SESSION['nombre_smp'] = $row['us_nombres'];
             $_SESSION['usuario_smp'] = $row['us_username'];
             $_SESSION['rol_smp'] = $row['ro_id'];
             $_SESSION['token_smp'] = md5(uniqid(mt_rand(), true));
-            return header("Location: " . SERVER_URL . "index.php?views=dashboard");
+            return 'ok';
         } else {
-            echo '
-                    <script>
-                        Swal.fire({
-                            title: "Ocurrio un error inesperado",
-                            text: "El USUARIO o CONTRASEÑA son incorrectos o su cuenta no esta activada!",
-                            icon: "error",
-                            confirmButtonText: "Aceptar"
-                        });
-                    </script>
-                    ';
-            exit();
+            $_SESSION['error_login'] = 'El USUARIO o CONTRASEÑA son incorrectos o su cuenta no esta activada!';
+            return 'error';
         }
     }
 
--- models/loginModel.php
+++ models/loginModel.php
@@ -9,7 +9,7 @@ class loginModel extends mainModel
     /* modelo para iniciar session */
     protected static function iniciar_sesion_model($datos)
     {
-        $sql = mainModel::conectar()->prepare("SELECT * FROM usuarios WHERE us_username = :Usuario AND us_password_hash = :Password AND us_estado = 1");
+        $sql = mainModel::Conectar()->prepare("SELECT * FROM usuarios WHERE us_username = :Usuario AND us_password_hash = :Password AND us_estado = 1");
         $sql->bindParam(":Usuario", $datos['usuario']);
         $sql->bindParam(":Password", $datos['password']);
         $sql->execute();
